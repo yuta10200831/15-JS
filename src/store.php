d24aad4126c8d5798a2b77b7f365c382c4660590
yuta10200831 <yfuse20ki@yahoo.co.jp>
2024-02-26 20:11:59 +0900

    最初から作り直し

--- src/store.php
+++ src/store.php
@@ -1,13 +1,11 @@
-<?php
+<?php 
 
 $title = filter_input(INPUT_POST, 'title');
 $text = filter_input(INPUT_POST, 'text');
-
 if ($title != '' && $text != '') {
 	$user = 'root';
 	$password = 'password';
 	$pdo = new PDO('mysql:host=mysql; dbname=blog; charset=utf8', $user, $password);
-
 	$sql = "INSERT INTO `blogs`(`id`, `title`, `content`) VALUES(0, :title, :content)";
 	$statement = $pdo->prepare($sql);
 	$statement->bindValue(':title', $title, PDO::PARAM_STR);
